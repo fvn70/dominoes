ebc31c41e9b7679324073759a674bfd1fed379c5
fvn70 <fvn_reg@mail.ru>
2022-02-07 18:23:57 +0700

    new Stage 3/5: Taking Turns

--- Dominoes/task/dominoes/dominoes.py
+++ Dominoes/task/dominoes/dominoes.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 
 def gen_set_game():
@@ -13,6 +14,69 @@ def get_max(list):
             imax = i
             vmax = list[i][0]
     return imax
+
+
+def draw_title(stock_size, comp_size):
+    print("=" * 70)
+    print(f"Stock size: {stock_size}")
+    print(f"Computer pieces: {comp_size}")
+
+
+def draw_player(list):
+    print("Your pieces:")
+    for i in range(len(list)):
+        print(f"{i + 1}:{list[i]}")
+    print()
+
+
+def draw_snake(list):
+    imax = len(list)
+    s = ""
+    for i in range(imax):
+        if i < 3 or i > imax - 4:
+            s += str(list[i])
+        if i == 2 and imax > 6:
+            s += "..."
+    print(f"\n{s}\n")
+
+
+def draw_prompt(t):
+    if t == 0:
+        print("Status: Computer is about to make a move. Press Enter to continue...")
+    else:
+        print("Status: It's your turn to make a move. Enter your command.")
+
+
+def end_game(comp, play, snake):
+    result = False
+    if len(comp) == 0:
+        print("Status: The game is over. The computer won!")
+        result = True
+    elif len(play) == 0:
+        print("Status: The game is over. You won!")
+        result = True
+    else:
+        if snake[0][0] == snake[len(snake) - 1][1]:
+            cnt = 0
+            for s in snake:
+                if s[0] == snake[0][0]:
+                    cnt += 1
+                if s[1] == snake[0][0]:
+                    cnt += 1
+            if cnt == 8:
+                print("Status: The game is over. It's a draw!")
+                result = True
+    return result
+
+
+def read_in(t, l):
+    while True:
+        cmd = input()
+        if t == 0:
+            return random.randint(-l, l)
+        if re.match('-?[0-9]+', cmd) and int(cmd) in range(-l, l + 1):
+            return int(cmd)
+        print("Invalid input. Please try again.")
 
 
 set_game = gen_set_game()
@@ -37,18 +101,21 @@ while True:
     turn = imax // 7  # 0 - "computer", 1 - "player"
     set_snake.append(set_players[turn].pop(imax % 7))
     turn = (turn + 1) % 2
-
     break
 
-print("=" * 70)
-print(f"Stock size: {len(set_stock)}")
-print(f"Computer pieces: {len(set_players[0])}\n")
-print(set_snake[0])
-print("\nYour pieces:")
-for i in range(1, len(set_players[1]) + 1):
-    print(f"{i}:{set_players[1][i - 1]}")
-print()
-if status[turn] == "computer":
-    print("Status: Computer is about to make a move. Press Enter to continue...")
-else:
-    print("Status: It's your turn to make a move. Enter your command.")
+while True:
+    draw_title(len(set_stock), len(set_players[0]))
+    draw_snake(set_snake)
+    draw_player(set_players[1])
+    if end_game(set_players[0], set_players[1], set_snake):
+        break
+    draw_prompt(turn)
+
+    i = read_in(turn, len(set_players[turn]))
+    if i == 0:
+        set_players[turn].append(set_stock.pop())
+    elif i > 0:
+        set_snake.append(set_players[turn].pop(i - 1))
+    else:
+        set_snake.insert(0, set_players[turn].pop(-i - 1))
+    turn = (turn + 1) % 2
