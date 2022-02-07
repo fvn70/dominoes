a518a9e8655aa31de3c46c0da30a566604502bf8
fvn70 <fvn_reg@mail.ru>
2022-02-07 23:27:01 +0700

    new Stage 5/5: The AI

--- Dominoes/task/dominoes/dominoes.py
+++ Dominoes/task/dominoes/dominoes.py
@@ -2,6 +2,29 @@ import random
 import re
 
 
+def calc_score():
+    update_count()
+    score = {}
+    i = 0
+    for p in set_players[0]:
+        k = count[p[0]] + count[p[1]]
+        score[i] = k
+        i += 1
+    s = [k for k in sorted(score, key=score.get)]
+    return s
+
+
+def update_count():
+    for i in range(7):
+        count[i] = 0
+    for p in set_snake:
+        count[p[0]] += 1
+        count[p[1]] += 1
+    for p in set_players[0]:
+        count[p[0]] += 1
+        count[p[1]] += 1
+
+
 def gen_set_game():
     return [[i, j] for i in range(7) for j in range(i, 7)]
 
@@ -22,19 +45,19 @@ def draw_title(stock_size, comp_size):
     print(f"Computer pieces: {comp_size}")
 
 
-def draw_player(list):
+def draw_player():
     print("Your pieces:")
-    for i in range(len(list)):
-        print(f"{i + 1}:{list[i]}")
+    for i in range(len(set_players[1])):
+        print(f"{i + 1}:{set_players[1][i]}")
     print()
 
 
-def draw_snake(list):
-    imax = len(list)
+def draw_snake():
+    imax = len(set_snake)
     s = ""
     for i in range(imax):
         if i < 3 or i > imax - 4:
-            s += str(list[i])
+            s += str(set_snake[i])
         if i == 2 and imax > 6:
             s += "..."
     print(f"\n{s}\n")
@@ -47,21 +70,21 @@ def draw_prompt(t):
         print("Status: It's your turn to make a move. Enter your command.")
 
 
-def end_game(comp, play, snake):
+def end_game():
     result = False
-    if len(comp) == 0:
+    if len(set_players[0]) == 0:
         print("Status: The game is over. The computer won!")
         result = True
-    elif len(play) == 0:
+    elif len(set_players[1]) == 0:
         print("Status: The game is over. You won!")
         result = True
     else:
-        if snake[0][0] == snake[len(snake) - 1][1]:
+        if set_snake[0][0] == set_snake[len(set_snake) - 1][1]:
             cnt = 0
-            for s in snake:
-                if s[0] == snake[0][0]:
+            for s in set_snake:
+                if s[0] == set_snake[0][0]:
                     cnt += 1
-                if s[1] == snake[0][0]:
+                if s[1] == set_snake[0][0]:
                     cnt += 1
             if cnt == 8:
                 print("Status: The game is over. It's a draw!")
@@ -72,29 +95,39 @@ def end_game(comp, play, snake):
 # 0 - p match value v
 # 1 - p need turn
 # -1 - do not match
-def compare(p, v):
+def check_snake(p, to_left, comp):
+    v = set_snake[0][0] if to_left else set_snake[len(set_snake) - 1][1]
     if p[0] != v and p[1] != v:
-        return -1
-    return 0 if p[0] == v else 1
+        result = -1
+    elif to_left:
+        result = 0 if p[1] == v else 1
+    else:
+        result = 0 if p[0] == v else 1
+    if not comp and result == -1:
+        print("Illegal move. Please try again.")
+    return result
 
 
 def read_in(t, l):
+    left = False
     if t == 0:
         # computer turn
         cmd = input()
         k = []
+        score = calc_score()
         while True:
-            i = random.randint(-l, l)
-            if i == 0 or len(k) == 2 * l + 1:
+            j = -1 if len(score) == 0 else score.pop()
+            if j == -1 or len(k) == 2 * l + 1:
                 return 0
-            if i in k:
+            if j in k:
                 continue
-            j = abs(i) - 1
+            k.append(j)
             p = set_players[0][j]
-            k.append(i)
-            right = compare(p, set_snake[len(set_snake) - 1][1])
-            left = compare(p, set_snake[0][0])
-            if i > 0 and right != -1 or i < 0 and left != -1:
+            chk = check_snake(p, left, True)
+            if chk == -1:
+                left = True
+                chk = check_snake(p, left, True)
+            if chk != -1:
                 break
     else:
         # player turn
@@ -106,29 +139,32 @@ def read_in(t, l):
                     return i
                 j = abs(i) - 1
                 p = set_players[1][j]
-                right = compare(p, set_snake[len(set_snake) - 1][1])
-                left = compare(p, set_snake[0][0])
-                if i > 0 and right == -1 or i < 0 and left == -1:
-                    print("Illegal move. Please try again.")
+                left = i < 0
+                chk = check_snake(p, left, False)
+                if chk != -1:
+                    break
+                else:
                     continue
-                break
             print("Invalid input. Please try again.")
 
-    j = abs(i) - 1
     p = set_players[t].pop(j)
-    if (i > 0 and right != 0) or (i < 0 and left == 0):
+    to_snake(p, left, chk == 1)
+    return 1
+
+
+def to_snake(p, left, reverse):
+    if reverse:
         p[0], p[1] = p[1], p[0]
-    if i > 0:
-        set_snake.append(p)
-    else:
+    if left:
         set_snake.insert(0, p)
-    return i
+    else:
+        set_snake.append(p)
 
 
-set_game = gen_set_game()
 set_snake = []
-status = ["computer", "player"]
 turn = 0
+count = {}
+
 
 while True:
     set_stock = gen_set_game()
@@ -151,9 +187,9 @@ while True:
 
 while True:
     draw_title(len(set_stock), len(set_players[0]))
-    draw_snake(set_snake)
-    draw_player(set_players[1])
-    if end_game(set_players[0], set_players[1], set_snake):
+    draw_snake()
+    draw_player()
+    if end_game():
         break
     draw_prompt(turn)
 
